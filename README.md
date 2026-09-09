3ae88557c31a90bab54570229627ebe1503ffc67
riot <claud214villar@gmail.com>
2026-09-10 00:37:14 +0800

    added user registration hashed

--- README.md
+++ README.md
@@ -3,9 +3,15 @@
 ## status:
 - [x] connected backend to PostgreSQL
 - [x] frontend can now fetch data from backend
-- [x] test table 'users' created in database
+- [x] tested table 'users' created in database
+- [x] added registration route (/api/users) with bcrypt hashing
+- [x] React registration form connected to backend (temporary ui)
 
 ## goal for this week
-- build the POST '/api/users' route to register a new user
-- build a simple react form to send an email/password to that route
-- save the user to the database
+~~- build the POST '/api/users' route to register a new user~~
+~~- build a simple react form to send an email/password to that route~~
+~~- save the user to the database~~
+- Build POST /api/auth/login route
+- Implement JWT token generation
+- Create login form in React
+- Protect routes with authentication middleware
--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const { Pool } = require('pg');
+
+const bcrypt = require('bcrypt'); // for hashing
+const express = require('express'); // for creating the server
+const cors = require('cors'); // for handling cross-origin reqs
+const { Pool } = require('pg'); // to connect to the pg db
 
 const app = express();
 app.use(cors());
@@ -29,3 +31,43 @@ app.get('/api/health', async (req, res) => {
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
+
+// register new user
+app.post('/api/register', async (req, res) => {
+  try {
+    // get the email and password from the whole app.use(express.json()) middleware
+    const { email, password } =  req.body;
+  
+    // if no email or password entered, return 400 error msg
+    if (!email || !password) {
+      return res.status(400).json({ error: 'Email and password required.'});
+    }
+
+    // check if user already exists in the db, if so return 409 error msg
+    const existingUser = await pool.query(
+      'SELECT * FROM users WHERE email = $1',
+      [email]
+    )
+    if (existingUser.rows.length > 0) {
+      return res.status(409).json({ error: 'User already exists.'});
+    }
+
+    // hash the password before storing using 10 salt round, then insert the new user into the db and return 201 success msg
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = await pool.query(
+      'INSERT INTO users (email, password_hash) VALUES ($1, $2) RETURNING id, email, created_at',
+      [email, hashedPassword]
+    )
+    res.status(201).json({
+      message: 'User registered successfully.',
+      user: newUser.rows[0]
+    })
+
+    // if error on our end, catch it and send 500 error msg
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+
+}
+)
